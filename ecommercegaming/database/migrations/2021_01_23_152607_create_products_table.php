2d35ebf16b0e6bb47f174829cd5da7072f0fbd6d
Marvin GOMES VITORINO <56402327+Gomerinho@users.noreply.github.com>
2021-01-23 16:56:32 +0100

    ajout de la table produit

---
+++ ecommercegaming/database/migrations/2021_01_23_152607_create_products_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Support\Str;
+
+class CreateProductsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('products', function (Blueprint $table) {
+            $table->id();
+            $table->string('img_product');
+            $table->string('name');
+            $table->string('desc');
+            $table->string('price');
+            $table->string('activate_code')->default(Str::random(16));
+            $table->integer('stock');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('products');
+    }
+}
